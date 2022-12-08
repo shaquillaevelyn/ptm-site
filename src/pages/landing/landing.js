c9f4bfd619e5f135e46bd5043e465f1baf70e5e7
shaquillaevelyn <s.evelynjohnson@outlook.com>
2022-12-08 11:54:00 +0000

    main cleanup

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import About from '../landing/about/about'
 import Jumbotron from './jumbo/jumbotron'
-import PageUp from '../../components/pageup/pageup'
 // import OnLoadModal from '../../components/on-load-modal/on-load-modal'
 import { QuoteOne, QuoteTwo } from './quotes/quotations'
 import Testimonials from '../landing/testimony/testimonial'
@@ -11,7 +10,6 @@ function Landing() {
     return (
         <>
             <Content>
-                <PageUp />
                 {/* <OnLoadModal /> */}
                 <Jumbotron />
                 <QuoteOne />
