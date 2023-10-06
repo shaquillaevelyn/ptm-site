b5e78fd1fe0249b1fd1b6eed28b8edc1d8bc4b02
shaquillaevelyn <s.evelynjohnson@outlook.com>
2023-10-06 21:44:01 +0100

    updated content component

--- src/components/utils/layout.js
+++ src/components/utils/layout.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { css } from '@emotion/css';
 
 function Content({ children }) {
   return (
     <div
-      className={css`
-                height: 100%;
-                width: 100vw;
-            `}
+      className='content'
     >
       {children}
     </div>
