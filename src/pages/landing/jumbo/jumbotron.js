60ab209046eaf36ca4903e0e3e302a7b2621cc3f
shaquilla <15138319+shaquillaevelyn@users.noreply.github.com>
2024-12-29 00:17:39 +0000

    removed autoplay for reduced motion (#35)

--- src/pages/landing/jumbo/jumbotron.js
+++ src/pages/landing/jumbo/jumbotron.js
@@ -3,37 +3,57 @@ import './jumbo.css';
 import backgroundvid from '../../../video/ptm-video.mp4';
 import { Button } from '../../../components/buttons/button';
 
+
 function Jumbotron() {
+
+  const prefersReduced = () => {
+    if( window.matchMedia('prefers-reduced')){
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // const properties = autoplay loop muted
+
   return (
-    <div className="jumbo margin-section-md">
-      <div className="background-video">
-        <video autoPlay loop muted id="video">
-          <source src={backgroundvid} type="video/mp4" />
-        </video>
-      </div>
-      <div className="overlay-desc">
-        <div className="jumbo-text">
-          <h1 className="header-text-mdlg">
+    <>
+      <div className="jumbo margin-section-md">
+        <div className="background-video">
+          { prefersReduced ? 
+            <video controls id="video">
+              <source src={backgroundvid} type="video/mp4" />
+            </video> :
+            <video autoPlay loop muted id="video">
+              <source src={backgroundvid} type="video/mp4" />
+            </video>
+
+          }
+        </div>
+        <div className="overlay-desc">
+          <div className="jumbo-text">
+            <h1 className="header-text-mdlg">
             We are Paddington Toastmasters
-            <br />
-            <span className="accent-text">A Club Like No Other</span>
-          </h1>
-          <p className="body-text-lg">
-            {' '}
+              <br />
+              <span className="accent-text">A Club Like No Other</span>
+            </h1>
+            <p className="body-text-lg">
+              {' '}
             Meetings every 1st and 3rd Tuesday of the Month
-            <br />
+              <br />
             18.00 for 18.15 start, 19.30 finish
-            {' '}
-            <br />
+              {' '}
+              <br />
             Westbourne Park Baptist Church, Porchester Rd,
             London W2 5DX
-            {' '}
-            <br />
-          </p>
-          <Button size='small' join={true}/>
+              {' '}
+              <br />
+            </p>
+            <Button size='small' join={true}/>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
