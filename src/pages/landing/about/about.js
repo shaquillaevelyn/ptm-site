f1913af53a01062134019fc32dbb5b9eeeb93268
shaquillaevelyn <s.evelynjohnson@outlook.com>
2025-04-07 19:37:18 +0100

    updated with text change

--- src/pages/landing/about/about.js
+++ src/pages/landing/about/about.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './about.css';
-import { Button, JoinButtonBig } from '../../../components/buttons/button';
+import { Button } from '../../../components/buttons/button';
 import about from '../../../text/about';
 
-
-
-
-export default function About(){
-
+export default function About() {
   return (
     <>
-
       <section className="margin-section-lg about-body">
         <article className="text">
           <h2 className="header-text-lg">
@@ -23,37 +18,38 @@ export default function About(){
           </p>
           <br />
           <p className="body-text-lg body-text-lg-bold">
-            It can be enjoyed and Paddington Toastmasters are here
-            to help!
+            It can be enjoyed and Paddington Toastmasters are here to help!
           </p>
           <br />
           <p className="body-text-lg">
-            We are a lively and supportive club, ready to support
-            you as you build your confidence, public speaking and
-            communication skills.Whether you are a complete newbie
-            or a seasoned public speaker, you are welcome to join us at
-            our meetings where we can improve together.
+            We are a lively and supportive club, ready to support you as you
+            build your confidence, public speaking and communication
+            skills.Whether you are a complete newbie or a seasoned public
+            speaker, you are welcome to join us at our meetings where we can
+            improve together.
           </p>
           <br />
           <p className="body-text-lg body-text-lg-bold">Progress with us</p>
           <p className="body-text-lg">
-            We pride ourselves on creating a relaxed and informal
-            environment where you feel comfortable to participate and
-            most importantly, have a laugh while doing so.&#10; On
-            becoming a member, we offer mentorship and support as you
-            make your way through the Toastmasters International
-            Educational Program, Pathways.
+            We pride ourselves on creating a relaxed and informal environment
+            where you feel comfortable to participate and most importantly, have
+            a laugh while doing so.&#10; On becoming a member, we offer
+            mentorship and support as you make your way through the Toastmasters
+            International Educational Program, Pathways.
           </p>
           <p className="body-text-lg">
             Learn more about
-            <span><a className="inline-link" href="www.bbc.co.uk">Pathways</a></span>
+            <span>
+              <a className="inline-link" href="www.bbc.co.uk">
+                Pathways
+              </a>
+            </span>
           </p>
           <br />
           <p className="body-text-lg body-text-lg-bold">Reap the Benefits </p>
-          <p id="test-ref" className="body-text-lg" >
+          <p id="test-ref" className="body-text-lg">
             Help us to help you to Build Leadership Skills,Maximise your
-            Potential, in the workplace and beyond, and Enhance
-            Self-Awareness.
+            Potential, in the workplace and beyond, and Enhance Self-Awareness.
             <br />
             The benefits are endless.
             <br />
@@ -62,18 +58,16 @@ export default function About(){
             <br />
             <br />
           </p>
-
         </article>
 
         <div>
-          <img id="image" src={about[0].image} alt='talking toastmasters' />;
+          <img id="image" src={about[0].image} alt="talking toastmasters" />;
         </div>
       </section>
 
-
       <div className="join-us">
-        <Button size={'large'} join={true}/>
+        <Button size={'large'} join={true} />
       </div>
     </>
   );
-};
+}
--- src/pages/landing/jumbo/jumbotron.js
+++ src/pages/landing/jumbo/jumbotron.js
@@ -3,11 +3,9 @@ import './jumbo.css';
 import backgroundvid from '../../../video/ptm-video.mp4';
 import { Button } from '../../../components/buttons/button';
 
-
 function Jumbotron() {
-
   const prefersReduced = () => {
-    if( window.matchMedia('prefers-reduced')){
+    if (window.matchMedia('prefers-reduced')) {
       return true;
     } else {
       return false;
@@ -20,41 +18,39 @@ function Jumbotron() {
     <>
       <div className="jumbo margin-section-md">
         <div className="background-video">
-          { prefersReduced ? 
+          {prefersReduced ? (
             <video controls id="video">
               <source src={backgroundvid} type="video/mp4" />
-            </video> :
+            </video>
+          ) : (
             <video autoPlay loop muted id="video">
               <source src={backgroundvid} type="video/mp4" />
             </video>
-
-          }
+          )}
         </div>
         <div className="overlay-desc">
           <div className="jumbo-text">
             <h1 className="header-text-mdlg">
-            We are Paddington Toastmasters
+              We are Paddington Toastmasters
               <br />
               <span className="accent-text">A Club Like No Other</span>
             </h1>
             <p className="body-text-lg">
               {' '}
-            Meetings every 1st and 3rd Tuesday of the Month
+              Meetings every 1st and 3rd Tuesday of the Month
               <br />
-            18.00 for 18.15 start, 19.30 finish
-              {' '}
+              18.00 for 18.15 start, 19.30 finish. <br />
               <br />
-            Westbourne Park Baptist Church, Porchester Rd,
-            London W2 5DX
-              {' '}
+              Westbourne Park Baptist Church <br />
+              Limited guest spaces!
               <br />
             </p>
-            <Button size='small' join={true}/>
+            <Button size="small" join={true} />
           </div>
         </div>
       </div>
     </>
   );
-};
+}
 
 export default Jumbotron;
