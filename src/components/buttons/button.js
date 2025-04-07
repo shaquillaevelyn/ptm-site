adf9c8ab358e1bd1440be45413bddf1efd59333f
shaquilla <15138319+shaquillaevelyn@users.noreply.github.com>
2025-04-07 19:38:41 +0100

    updated with text change (#36)

--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
-
-export function Button({size, join}){
-
-  const emailJoin = 'mailto:janetfmmilnes@gmail.com?subject=I%20would%20like%20to%20join%20a%20PTM%20meeting!';
-  const emailQuery = 'mailto:janetfmmilnes@gmail.com?subject=I%20would%20like%20to%20know%20more%20about%20PTM%20meetings!';
+export function Button({ size, join }) {
+  const emailJoin =
+    'mailto:janetfmmilnes@gmail.com?subject=I%20would%20like%20to%20join%20a%20PTM%20meeting!';
+  const emailQuery =
+    'mailto:janetfmmilnes@gmail.com?subject=I%20would%20like%20to%20know%20more%20about%20PTM%20meetings!';
 
   let emailText = '';
   let buttonType = '';
   let buttonSize = '';
 
-  if(size === 'small'){
+  if (size === 'small') {
     buttonSize = 'btn-sm ';
-
   } else if (size === 'large') {
     buttonSize = 'btn-lg ';
   } else {
     buttonSize = 'btn-md ';
   }
 
-  if (join){
+  if (join) {
     emailText === emailJoin;
     buttonType = 'join-btn ';
   } else {
@@ -27,10 +26,12 @@ export function Button({size, join}){
     buttonType = 'query-btn ';
   }
 
-  return(
-    <a className={buttonSize + buttonType + 'btn'} href={join ? emailJoin : emailQuery}>
-      <span>{join ? 'Join us at our next meeting' : ' Drop us an email'}</span>
-    </a>
-
+  return (
+    <button
+      className={buttonSize + buttonType + 'btn'}
+      href={join ? emailJoin : emailQuery}
+    >
+      <span>{join ? 'Register Interest' : ' Drop us an email'}</span>
+    </button>
   );
 }
--- src/pages/landing/contact-form/contact-form.jsx
+++ src/pages/landing/contact-form/contact-form.jsx
@@ -2,69 +2,43 @@
 import React from 'react';
 import { Button } from '../../../components/buttons/button';
 
-function ContactForm(){
-
-
-  // let meetingDate = new Date();
-
-  // let meetingYear = meetingDate.getYear();
-  // let meetingMonth = meetingDate.getMonth();
-  // let meetingDay = meetingDate.getDay();
-  // let tuesday = [];
-
-  // let meetingTuesday = [];
-
-  // meetingDate= setDate(1);
-
-  // while (meetingDate.getDay() !== 1) {
-  //   meetingDate.setDate(meetingDate.getDate() + 1);
-  // }
-
-  // while (meetingDate.getYear() === meetingYear) {
-  //   var pushDate = new Date(meetingDate.getTime());
-  //   tuesday.push(pushDate.getDate() + '-' + (pushDate.getMonth()+1) + '-' + pushDate.getFullYear());
-  //   meetingDate.setDate(d.getDate() + 7);
-  // }
-
-  // console.log(tuesday);
-
-  // if( )
-  // if (meetingMonth.getDay === 2){
-  //   console.log();
-  // }
-  // let days = meetingDate(year, month, 1);
-
-
-  // console.log(meetingYear);
-  // console.log(days);
-
+function ContactForm() {
   return (
     <>
-
-
-      <p id='landing-contact' className='chunky-body-text margin-section-sm flex-row'> Ready to take the leap?</p>
-      <div className='margin-section-sm about-body'>
+      <p
+        id="landing-contact"
+        className="chunky-body-text margin-section-sm flex-row"
+      >
+        {' '}
+        Ready to take the leap?
+      </p>
+      <div className="margin-section-sm about-body">
         <section className="margin-section-lg about-body-col">
-       
           <p className="header-text-mdlg">Join us at our next meeting!</p>
-          <p className="body-text-lg">Westbourne Park Baptist Church, Porchester Rd, London W2 5DX
+          <p className="body-text-lg">
+            Westbourne Park Baptist Church, Porchester Rd, London W2 5DX
           </p>
           <p className="body-text-lg">
-           18.00 for 18.15 start, 19.30 finish
-          </p> 
-          {' '}
-          <Button size='small' join={true}/>
-
+            Limited guest spaces!
+            <br />
+            Reach out to Janet using the button below to register your interest.
+          </p>{' '}
+          <Button size="small" join={true} />
         </section>
-        <section className="margin-section-lg about-body-col">  
-          <iframe className="iframe"title='paddington toastmasters location' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9930.821114971737!2d-0.1891393!3d51.5186228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487611287d473703%3A0x53fa99ac788a9369!2sPaddington%20Toastmasters!5e0!3m2!1sen!2suk!4v1700836116875!5m2!1sen!2suk" style={{border:0}} allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <section className="margin-section-lg about-body-col">
+          <iframe
+            className="iframe"
+            title="paddington toastmasters location"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9930.821114971737!2d-0.1891393!3d51.5186228!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487611287d473703%3A0x53fa99ac788a9369!2sPaddington%20Toastmasters!5e0!3m2!1sen!2suk!4v1700836116875!5m2!1sen!2suk"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </section>
       </div>
-
-
-     
     </>
   );
-};
+}
 
 export default ContactForm;
